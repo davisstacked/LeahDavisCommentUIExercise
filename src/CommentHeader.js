5f77f02c84b3682763459b56f0775a6247965d64
davisstacked <brettzvi@gmail.com>
2021-12-03 14:15:35 -0800

    created comment components

---
+++ src/CommentHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CommentHeader = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CommentHeader;
---
+++ src/CommentList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CommentList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CommentList;
+
