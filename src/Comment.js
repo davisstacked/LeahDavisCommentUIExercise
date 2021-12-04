c99962a83e0be963fa1c6152666526436b793f22
davisstacked <brettzvi@gmail.com>
2021-12-03 22:55:19 -0800

    added time from now formatting for dates

--- src/Comment.js
+++ src/Comment.js
@@ -2,21 +2,23 @@ import React from 'react';
 import { pink } from '@mui/material/colors';
 import Avatar from '@mui/material/Avatar';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import moment from 'moment';
+
+import './Comment.css'
 
 
 const Comment = ({time, message, user, image}) => {
 
   return (
-    <div>
+    <div className="Comment">
       <Avatar alt={user} src={image} sx={{ width: 40, height: 40 }} />
       {/* <Avatar sx={{ bgcolor: pink[500] }}>
         <CameraAltIcon />
       </Avatar> */}
 
       <div className='Comment-comment'>
-        <p>
-          {user}∙{time}
-        </p>
+        <p>{user}∙{moment(time).fromNow()}</p>
+        <p>{message}</p>
       </div>
     </div>
   );
